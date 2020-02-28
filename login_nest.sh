45de7a7ab810c3620c941f445593c69cc581cd17
xkia <xukai8023@gmail.com>
2020-02-28 14:58:14 +0800

    兼容哗啦啦jumper快速输入

--- login_nest.sh
+++ login_nest.sh
@@ -12,4 +12,11 @@ s_port=$6
 s_user=$7
 s_password=$8
 
-./auto_login_jumper.exp $host $port $user $password $s_host $s_port $s_user $s_password
+if [ "$5 == 'MFA'" ]; then
+    oath=`./oath.sh`
+    echo $host $port $user $password $oath $s_host
+    ./auto_go.exp $host $port $user $password $oath $s_host
+else 
+  echo "111"
+#    ./auto_login_jumper.exp $host $port $user $password $s_host $s_port $s_user $s_password
+fi
